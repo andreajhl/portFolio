65cbe23a18e7124d3ea2aa9e538692307619b175
Andrea Hernandez <andreajhl29@gmail.com>
2024-02-29 12:48:01 -0300

    add constants

---
+++ src/constants/projects.js
@@ -0,0 +1,34 @@
+export const projectCard = {
+  mercadoLibre: '/assets/img/mercado-libre/meli.png',
+  ficohsa: '/assets/img/e-commerce/e-commerce.png',
+  famosos: '/assets/img/famosos/famosos.png',
+};
+
+export const projectsCarousel = {
+  mercadoLibre: [
+    {
+      url: '/assets/img/mercado-libre/meli.png',
+      alt: 'banner market application shipments',
+    },
+  ],
+  ficohsa: [
+    {
+      url: '/assets/img/e-commerce/e-commerce.gif',
+      alt: 'video ecommerce of books',
+    },
+  ],
+  famosos: [
+    {
+      url: '/assets/img/famosos/famosos-1.png',
+      alt: 'celebrity homepage photo',
+    },
+    {
+      url: '/assets/img/famosos/famosos-2.png',
+      alt: 'photo purchase confirmation page',
+    },
+    {
+      url: '/assets/img/famosos/famosos-3.png',
+      alt: 'photo category page',
+    },
+  ],
+};
