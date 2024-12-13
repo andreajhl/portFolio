625892479ab57ffb8f37189c468008c0b6cb0625
Andrea Hernandez <andrea.hernandez@adgoat.io>
2024-12-13 19:19:12 -0300

    feat: :wrench: update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': resolve(__dirname, 'src/components'),
+      '@constants': resolve(__dirname, 'src/constants'),
+      '@ui': resolve(__dirname, 'src/components/ui'),
+      '@wordings': resolve(__dirname, 'src/wordings'),
+      '@styles': resolve(__dirname, 'src/styles'),
+    };
+
+    config.resolve.extensions = ['.js', '.jsx'];
+
+    return config;
+  },
+};
 
 export default nextConfig;
