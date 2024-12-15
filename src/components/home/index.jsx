9fbbf0e9972dd728e7ea1d99950cf9c22bc211a1
Andrea Hernandez <andrea.hernandez@adgoat.io>
2024-12-15 04:50:40 -0300

    feat: :lipstick: Update Home UI

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import wordings from '../../wordings';
-import './styles.scss'
+import wordings from '@wordings';
+import Image from 'next/image';
+import Link from 'next/link';
+import './styles.scss';
 
-const Home = () => (
-  <section className='home'>
-    <div className="home-container">
-      <div className="home-container__wording">
-        <h2 className="home-container__wording-name">{wordings.home.title}</h2>
-        <h4 className="home-container__wording-position">{wordings.home.subtitle}</h4>
-        <button className="home-container__wording-curriculum">
-          {wordings.home.button}
-        </button>
+const Home = () => {
+  const { home: { title, subtitle, cv } } = wordings;
+
+  return (
+    <div className='home'>
+      <div className='home-container'>
+        <div className='home-text'>
+          <h2 className='home-name'>{title}</h2>
+          <h4 className='home-position'>{subtitle}</h4>
+          <Link
+            href='https://drive.google.com/file/d/1cwyPj3nJSE9oJQb6jS-AN6uX1Pzlp3Du/view?usp=sharing'
+            rel='noopener noreferrer'
+            aria-label='Go to curriculum'
+            target='_blank'
+            className='home-curriculum'
+          >
+            {cv}
+          </Link>
+        </div>
+        <div className='home-img'>
+          <Image
+            fill
+            priority
+            decoding='async'
+            alt='Phone personal'
+            src='/assets/img/user.png'
+            className='home-personal-img'
+          />
+        </div>
       </div>
-      <div className="home-container__img">
-        <img decoding="async" src="https://dlmak.backdt.com/wp-content/uploads/2023/05/hero_img-1.png" alt="Mak" className="personal-img" />
-      </div>      
     </div>
-  </section>
-);
+  );
+};
 
 export default Home;
