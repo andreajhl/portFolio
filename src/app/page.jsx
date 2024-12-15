5a7fb5241e96197064744655a3beee2f61d9b10d
Andrea Hernandez <andrea.hernandez@adgoat.io>
2024-12-15 04:57:30 -0300

    refactor: :recycle: refactor page main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,26 @@
-import PageMain from '@/components/page-main';
+import About from '@components/about';
+import Contact from '@components/contact';
+import Home from '@components/home';
+import Projects from '@components/projects';
+import Skill from '@components/skill';
 
 const Portfolio = () => (
-  <main className='container'>
-    <PageMain />
+  <main className='main'>
+    <section id='home'>
+      <Home />
+    </section>
+    <section id='about'>
+      <About />
+    </section>
+    <section id='skill'>
+      <Skill />
+    </section>
+    <section id='projects'>
+      <Projects />
+    </section>
+    <section id='contact'>
+      <Contact />
+    </section>
   </main>
 );
 
