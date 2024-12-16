259f904fb56bde54e37aa8b59585dfea420d7190
Andrea Hernandez <andrea.hernandez@adgoat.io>
2024-12-16 05:00:55 -0300

    remove: remove icons unused

--- src/components/ui/icons/index.jsx
+++ src/components/ui/icons/index.jsx
@@ -1,96 +1,35 @@
 /* eslint-disable react/jsx-key */
-import { DiGoogleAnalytics, DiScrum } from 'react-icons/di';
-import { FaJira } from 'react-icons/fa';
 import {
-  IoLogoNodejs,
-  IoLogoCss3,
   IoLogoHtml5,
 } from 'react-icons/io';
 import {
   SiJavascript,
   SiTypescript,
-  SiNewrelic,
-  SiExpress,
   SiReact,
-  SiTrello,
   SiJest,
   SiGithub,
-  SiBootstrap,
   SiSass,
   SiRedux,
-  SiMui,
-  SiPython,
   SiTailwindcss,
-  SiCypress,
   SiLinkedin,
-  SiFigma,
   SiGmail,
 } from 'react-icons/si';
-import { TbBrandNextjs } from 'react-icons/tb';
 
-const languages = [
+const technologies = [
   <SiJavascript />,
   <SiTypescript />,
-  <SiPython />,
-  <IoLogoHtml5 />,
-];
-
-const frameworks = [
   <SiReact />,
   <SiRedux />,
-  <TbBrandNextjs />,
-  <IoLogoNodejs/>,
-];
-
-const styles = [
-  <IoLogoCss3 />,
+  <IoLogoHtml5 />,
   <SiSass />,
   <SiTailwindcss />,
-  <SiMui />,
-  <SiBootstrap />,
-];
-
-const others = [
   <SiJest />,
-  <SiCypress />,
-  <DiGoogleAnalytics />,
 ];
-
-const toolIcons = [
-  <SiGithub />,
-  <SiFigma />,
-  <FaJira />,
-  <DiScrum />,
-];
-
-const techIcons = {
-  languages,
-  frameworks,
-  styles,
-  others,
-};
 
 export {
-  SiJavascript,
-  SiTypescript,
-  SiNewrelic,
-  SiExpress,
-  SiReact,
-  SiTrello,
-  SiJest,
   SiGithub,
-  SiBootstrap,
-  SiSass,
-  IoLogoNodejs,
-  IoLogoCss3,
-  IoLogoHtml5,
-  DiGoogleAnalytics,
-  DiScrum,
-  TbBrandNextjs,
-  FaJira,
   SiLinkedin,
   SiGmail,
-  techIcons,
-  toolIcons,
+  technologies,
 };
 /* eslint-disable react/jsx-key */
